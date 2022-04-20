e64e77c6451e4e7e77648471d2543d35a65ca58a
eetann <hideharu092@gmail.com>
2022-04-20 21:05:23 +0900

    fix: zIndex

--- src/content/App.tsx
+++ src/content/App.tsx
@@ -1,6 +1,18 @@
+import { Rnd } from "react-rnd";
+
 function App() {
   return (
-    <div>hogehhhhhhhhhh</div>
+    <Rnd
+      className="bg-blue-200"
+      default={{
+        x: 0,
+        y: 0,
+        width: 320,
+        height: 200,
+      }}
+    >
+      <div className="shadow-lg bg-red-100">hogehhhhhhhhhh</div>
+    </Rnd>
   );
 }
 export default App;
--- src/content/index.tsx
+++ src/content/index.tsx
@@ -5,6 +5,10 @@ import ReactDOM from "react-dom/client";
 
 const choomameRoot = document.createElement("div");
 choomameRoot.id = "choomameRoot";
+choomameRoot.style.zIndex = "999";
+choomameRoot.style.position = "fixed";
+choomameRoot.style.top = "0";
+choomameRoot.style.left = "0";
 
 document.body.appendChild(choomameRoot);
 
