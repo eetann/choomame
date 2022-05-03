333563cb8ed39679a43c89dcf56469d9932e07ca
eetann <hideharu092@gmail.com>
2022-05-03 14:15:43 +0900

    show data from chrome.storage.local via redux store #1

--- src/options/TabTimes.tsx
+++ src/options/TabTimes.tsx
@@ -1,9 +1,60 @@
-import React, { useEffect, useState } from "react";
-import { Box, Text } from "@chakra-ui/react";
+import type { AppDispatch } from "../app/store";
+import {
+  removeOneTime,
+  selectAllTimes,
+  setAllTimes,
+  Time,
+} from "../features/time/timeSlice";
+import { DeleteIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  TableContainer,
+  Table,
+  Tbody,
+  Tr,
+  Td,
+  IconButton,
+} from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const TabTimes: React.VFC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const times = useSelector(selectAllTimes);
+
+  useEffect(() => {
+    dispatch(setAllTimes());
+  }, [dispatch]);
+
   return (
-    <Box>aaa</Box>
+    <Box>
+      <TableContainer rounded="lg" boxShadow="xs" maxW="min-content">
+        <Table variant="simple">
+          <Tbody>
+            {times.reduce((prev_array: JSX.Element[], time: Time) => {
+              if (time.timeId === "all") {
+                return prev_array;
+              }
+              prev_array.push(
+                <Tr key={time.timeId}>
+                  <Td fontSize="md">
+                    {time.number.toString() + " " + time.unit}
+                  </Td>
+                  <Td>
+                    <IconButton
+                      aria-label="Delete time"
+                      icon={<DeleteIcon />}
+                      onClick={() => dispatch(removeOneTime(time.timeId))}
+                    />
+                  </Td>
+                </Tr>
+              );
+              return prev_array;
+            }, [])}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 };
 export default TabTimes;
