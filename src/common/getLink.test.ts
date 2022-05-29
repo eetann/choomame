b1b106fd51b3a4fd061dd152be1979ad7daea83e
eetann <hideharu092@gmail.com>
2022-05-29 13:22:09 +0900

    add test case samples #5

--- src/common/getLink.test.ts
+++ src/common/getLink.test.ts
@@ -1,15 +1,45 @@
-import { expect, test } from 'vitest'
 import { Param } from "../features/param/paramSlice";
 import { getLink } from "./getLink";
+import { describe, expect, test } from "vitest";
 
-test('make link with parameter', () => {
-  const param:Param = {
-    url: "https://www.google.com/search?q=kerry",
-    q: "kerry",
-    tbs: "",
-    lr: "",
-    tbm: "",
-    qLink: "https://www.google.com/search?q=kerry",
-  };
-  expect(getLink(param)).toBe("https://www.google.com/search?q=kerry");
-})
+describe("generate link", () => {
+  test("without parameters", () => {
+    const param: Param = {
+      url: "https://www.google.com/search?q=kerry",
+      q: "kerry",
+      tbs: "",
+      lr: "",
+      tbm: "",
+      qLink: "https://www.google.com/search?q=kerry",
+    };
+    expect(getLink(param)).toBe("https://www.google.com/search?q=kerry");
+  });
+
+  test("with parameters", () => {
+    const param: Param = {
+      url: "https://www.google.com/search?q=kerry",
+      q: "kerry",
+      tbs: "qdr:y1",
+      lr: "lang_ja",
+      tbm: "",
+      qLink: "https://www.google.com/search?q=kerry",
+    };
+    expect(getLink(param)).toBe(
+      "https://www.google.com/search?q=kerry&tbs=qdr:y1&lr=lang_ja"
+    );
+  });
+
+  test("with tbm parameter", () => {
+    const param: Param = {
+      url: "https://www.google.com/search?q=kerry",
+      q: "kerry",
+      tbs: "qdr:y1",
+      lr: "lang_ja",
+      tbm: "isch",
+      qLink: "https://www.google.com/search?q=kerry",
+    };
+    expect(getLink(param)).toBe(
+      "https://www.google.com/search?q=kerry&tbs=qdr:y1&lr=lang_ja&tbm=isch"
+    );
+  });
+});
