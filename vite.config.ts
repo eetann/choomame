39b4ce37e2d0e1e926120d1ab0b25d3072dd5681
eetann <hideharu092@gmail.com>
2022-07-04 20:21:05 +0900

    replace manifest.json by ts to #27

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import manifest from "./src/manifest";
 import { crx } from "@crxjs/vite-plugin";
-import manifest from './src/manifest.json'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), crx({ manifest })],
