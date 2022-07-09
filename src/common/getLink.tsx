88203214b7ee2afd52d1fc75f5eb43c62bcf9055
eetann <hideharu092@gmail.com>
2022-07-09 16:28:14 +0900

    change tbs link for time value of 1 #31

--- src/common/getLink.tsx
+++ src/common/getLink.tsx
@@ -1,5 +1,5 @@
 import { Language } from "../features/languages/languagesSlice";
-import { joinTbs, Param } from "../features/param/paramSlice";
+import { joinTbs, Param, ParamTbs } from "../features/param/paramSlice";
 import { Time } from "../features/times/timesSlice";
 
 export function getLink(
@@ -10,16 +10,26 @@ export function getLink(
   const url = new URL(param.url);
   const new_param = new URLSearchParams();
   new_param.set("q", param.q);
-  if (typeof time !== "undefined") {
-    if (time.timeId !== "Any") {
-      new_param.set("tbs", "qdr:" + time.timeId);
-    }
-    // time.timeId === "Any" empty
-  } else if (Object.keys(param.tbs).length !== 0) {
-    new_param.set("tbs", joinTbs(param.tbs));
-  }
-  // else empty
 
+  // Time
+  const newTbs: ParamTbs = {};
+  Object.assign(newTbs, param.tbs);
+  if (typeof time !== "undefined") {
+    if (time.timeId === "Any") {
+      delete newTbs.qdr;
+    } else {
+      let newQdr = time.timeId;
+      if (time.number === 1) {
+        newQdr = time.timeId.charAt(0);
+      }
+      newTbs.qdr = newQdr;
+    }
+  }
+  if (Object.keys(newTbs).length !== 0) {
+    new_param.set("tbs", joinTbs(newTbs));
+  }
+
+  // Language
   if (typeof language !== "undefined") {
     if (language !== "Any") {
       new_param.set("lr", language);
