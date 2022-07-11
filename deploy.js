43fb8f0335982b5e3ac45d77d39b8511c82e3266
eetann <hideharu092@gmail.com>
2022-07-11 22:03:59 +0900

    add meta data and packages for publish #30

---
+++ deploy.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const zipFolder = require('zip-folder');
+let folderName = './dist';
+
+let zipName = 'choomame.zip';
+
+zipFolder(folderName, zipName, function(err) {
+	if(err) {
+		console.log('oh no!', err);
+	} else {
+    console.log(`Successfully zipped the ${folderName} directory and store as ${zipName}`);
+	}
+});
