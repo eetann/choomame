142bb98ab562021179873f840364f6ff41b6b382
eetann <hideharu092@gmail.com>
2022-11-23 15:41:52 +0900

    add validation using zod #51

--- src/features/customLinks/customLinks.ts
+++ src/features/customLinks/customLinks.ts
@@ -1,38 +1,63 @@
 import { getBucket } from "@extend-chrome/storage";
 import JSON5 from "json5";
-import { RiTyphoonFill } from "react-icons/ri";
+import { z } from "zod";
 
-export type CustomLink = {
-  kind: string;
-  url: string;
-  enable: boolean;
-};
+const customLinkSchema = z.object({
+  kind: z.string().min(1).max(50),
+  url: z.string().url().max(200),
+  enable: z.boolean(),
+});
 
-export type CustomLinkItem = {
-  list_id: string;
-  target: string;
-  hit: string;
-  links: CustomLink[];
-};
+const customLinkListIdSchema = z
+  .string()
+  .min(1)
+  .max(50)
+  .regex(/[A-Za-z0-9_]/);
 
-export type CustomLinkItems = Record<string, CustomLinkItem>;
+const customLinkItemSchema = z.object({
+  list_id: customLinkListIdSchema,
+  target: z.string().min(1).max(50),
+  hit: z.string().refine(
+    (value: string) => {
+      try {
+        new RegExp(value);
+      } catch (e) {
+        return false;
+      }
+      return true;
+    },
+    (value: string) => ({
+      message: `${value} is not not a valid regular expression`,
+    })
+  ),
+  links: z.array(customLinkSchema),
+});
 
-export type CustomLinkListInfo = {
-  name: string;
-  url: string;
-};
+const customLinkItemsSchema = z.record(customLinkItemSchema);
 
-export type CustomLinkList = Record<string, CustomLinkListInfo>;
+type CustomLinkItems = z.infer<typeof customLinkItemsSchema>;
+
+const customLinkListSchema = z.record(
+  z.object({
+    name: z.string().min(1).max(50),
+    url: z.string().url().max(150),
+  })
+);
+
+type CustomLinkList = z.infer<typeof customLinkListSchema>;
 
 export type CustomLinkItemsBucket = {
   list: CustomLinkList;
   items: CustomLinkItems;
 };
 
-export type FetchCustomLinkUrl = {
-  id: string;
-  name: string;
-} & { items: CustomLinkItems };
+const fetchCustomLinkUrlSchema = z.object({
+  id: customLinkListIdSchema,
+  name: z.string().min(1).max(50),
+  items: customLinkItemsSchema,
+});
+
+export type FetchCustomLinkUrl = z.infer<typeof fetchCustomLinkUrlSchema>;
 
 export const customLinkBucket = getBucket<CustomLinkItemsBucket>("customLink");
 
@@ -45,35 +70,41 @@ export const initialCustomLinkUrls = [
 
 export async function fetchCustomLinkUrl(
   url: string
-): Promise<FetchCustomLinkUrl | null> {
-  let response = {};
+): Promise<FetchCustomLinkUrl> {
+  let response;
   try {
-    response = JSON5.parse<FetchCustomLinkUrl>(await (await fetch(url)).text());
+    response = await (await fetch(url)).text();
   } catch (e) {
-    console.warn(`fetch failed: ${url}`);
-    return null;
+    throw new Error(`fetch failed: ${url}`);
   }
-  // ここでスキーマチェック
-  return null;
+  try {
+    response = JSON5.parse<FetchCustomLinkUrl>(response);
+  } catch (e) {
+    throw new Error(`JOSN5 parse Error: ${url}`);
+  }
+  return fetchCustomLinkUrlSchema.parse(response);
 }
 
 export async function customLinkOnInstalled() {
   const listBucket = await customLinkBucket.get("list");
   if (Object.keys(listBucket).length === 0) {
     for (const customLinkUrl of initialCustomLinkUrls) {
-      const response = await fetchCustomLinkUrl(customLinkUrl);
-      if (response != null) {
-        // TODO: チェックの関数を入れる
-        customLinkBucket.set({
-          list: {
-            [response.id]: {
-              name: response.name,
-              url: customLinkUrl,
-            },
-          },
-          items: response.items,
-        });
+      let response;
+      try {
+        response = await fetchCustomLinkUrl(customLinkUrl);
+      } catch (e) {
+        console.log(e);
+        continue;
       }
+      customLinkBucket.set({
+        list: {
+          [response.id]: {
+            name: response.name,
+            url: customLinkUrl,
+          },
+        },
+        items: response.items,
+      });
     }
     customLinkBucket.set({ list: {}, items: {} });
   }
