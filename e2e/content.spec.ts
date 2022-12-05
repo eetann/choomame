47c8379daeb30673091c594e47474c538f838f7e
eetann <hideharu092@gmail.com>
2022-12-05 21:22:48 +0900

    fix E2E test to work #51

--- e2e/content.spec.ts
+++ e2e/content.spec.ts
@@ -1,8 +1,14 @@
-import { test, expect } from "./fixtures.ts";
+import { test, expect } from "./fixtures.js";
+import { initialLanguagesStorage } from "../src/features/languages/languages.js";
 
 test('example test', async ({ page }) => {
-  await page.goto('https://example.com');
-  await expect(page.locator('body')).toHaveText(/example/);
+  // onInstalled
+  await page.waitForTimeout(3000)
+
+  await page.goto('https://www.google.com/search?q=hoge');
+  for (const language of ["Any", "English", "Japanese"]) {
+    await expect(page.locator('#choomameRoot')).toHaveText(new RegExp(language));
+  }
 });
 
 // test('popup page', async ({ page, extensionId }) => {
