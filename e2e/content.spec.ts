9025d1de23daba8275a0b4f0d87b1277f589b3c4
eetann <hideharu092@gmail.com>
2022-12-10 20:53:56 +0900

    add: test for Times #51

--- e2e/content.spec.ts
+++ e2e/content.spec.ts
@@ -42,3 +42,34 @@ test('Languages test', async ({ page, extensionId }) => {
     await expect(page.locator('#choomameRoot')).toHaveText(new RegExp(language));
   }
 });
+
+
+test('Times test', async ({ page, extensionId }) => {
+  // add new time
+  await page.goto(`chrome-extension://${extensionId}/index.html`);
+  await page.locator("_react=App >> text='Time'").click()
+  await page.locator("_react=TimesForm >> #timeNumber").fill('3')
+  await page.locator("_react=TimesForm >> select").selectOption({label:"day"})
+  await page.locator("_react=TimesForm >> _react=[aria-label = 'Add time']").click()
+
+  // remove time
+  await page.locator("_react=TimesTable >> tr:has-text('6 month') >> _react=[aria-label = 'Delete time']").click()
+
+  // check
+  await page.goto('https://www.google.com/search?q=hoge');
+  for (const time of ["Any", "1 day", "3 day", "1 week", "1 month", "1 year", "3 year"]) {
+    await expect(page.locator('#choomameRoot')).toHaveText(new RegExp(time));
+  }
+
+  // reset times
+  await page.goto(`chrome-extension://${extensionId}/index.html`);
+  await page.locator("_react=App >> text='Time'").click();
+  await page.locator("_react=TimesReset >> text='Reset'").click();
+  await page.locator("text='Yes, reset.'").click();
+
+  // check
+  await page.goto('https://www.google.com/search?q=hoge');
+  for (const time of ["Any", "1 day", "1 month", "6 month", "1 week", "3 year"]) {
+    await expect(page.locator('#choomameRoot')).toHaveText(new RegExp(time));
+  }
+});
