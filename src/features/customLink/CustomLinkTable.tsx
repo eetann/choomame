31be414fbd4451eae4924dbffda3dbfa19c9c0fc
eetann <hideharu092@gmail.com>
2023-01-09 18:19:42 +0900

    fix: table width for customLink #75

--- src/features/customLink/CustomLinkTable.tsx
+++ src/features/customLink/CustomLinkTable.tsx
@@ -16,17 +16,27 @@ import {
   Th,
   Link,
   Switch,
+  VStack,
+  HStack,
+  Button,
 } from "@chakra-ui/react";
 import { AnyAction } from "@reduxjs/toolkit";
 import {
   createColumnHelper,
   flexRender,
   getCoreRowModel,
+  getPaginationRowModel,
   RowData,
   useReactTable,
 } from "@tanstack/react-table";
 import React from "react";
 import { HiOutlineTrash } from "react-icons/hi";
+import {
+  HiOutlineChevronDoubleLeft,
+  HiOutlineChevronDoubleRight,
+  HiOutlineChevronLeft,
+  HiOutlineChevronRight,
+} from "react-icons/hi2";
 import { useDispatch, useSelector } from "react-redux";
 
 declare module "@tanstack/table-core" {
@@ -45,8 +55,9 @@ const columns = [
     cell: (info) => info.getValue(),
     header: "Group name",
     meta: {
-      props: {
-        maxWidth: 64,
+      tdProps: {
+        width: 48,
+        wordBreak: "break-word",
       },
     },
   }),
@@ -54,8 +65,9 @@ const columns = [
     cell: (info) => info.getValue(),
     header: "Match",
     meta: {
-      props: {
-        maxWidth: 64,
+      tdProps: {
+        width: 56,
+        wordBreak: "break-word",
       },
     },
   }),
@@ -63,8 +75,9 @@ const columns = [
     cell: (info) => info.getValue(),
     header: "Link name",
     meta: {
-      props: {
-        maxWidth: 64,
+      tdProps: {
+        width: 40,
+        wordBreak: "break-word",
       },
     },
   }),
@@ -79,8 +92,9 @@ const columns = [
     },
     header: "URL",
     meta: {
-      props: {
-        maxWidth: 80,
+      tdProps: {
+        width: 72,
+        wordBreak: "break-word",
       },
     },
   }),
@@ -92,8 +106,8 @@ const columns = [
       return table.options.meta?.getListName(list_id);
     },
     meta: {
-      props: {
-        maxWidth: 64,
+      thProps: {
+        width: 36,
       },
     },
   }),
@@ -120,7 +134,7 @@ const columns = [
     },
     header: "",
     meta: {
-      props: {
+      tdProps: {
         height: 14,
         pr: 1,
       },
@@ -138,6 +152,7 @@ const CustomLinkTable: React.FC = () => {
     data: customLinks,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
     meta: {
       removeCustomLink: (id: string) => dispatch(removeManyCustomLinks([id])),
       toggleCustomLink: (customLink: CustomLink) =>
@@ -147,44 +162,81 @@ const CustomLinkTable: React.FC = () => {
   });
 
   return (
-    <TableContainer
-      rounded="md"
-      boxShadow="xs"
-      maxW="min-content"
-      whiteSpace="normal"
-    >
-      <Table variant="simple" width="80%">
-        <Thead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <Tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <Th key={header.id}>
-                  {flexRender(
-                    header.column.columnDef.header,
-                    header.getContext()
-                  )}
-                </Th>
-              ))}
-            </Tr>
-          ))}
-        </Thead>
-        <Tbody>
-          {table.getRowModel().rows.map((row) => (
-            <Tr key={row.id} minHeight="96">
-              {row.getVisibleCells().map((cell) => {
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                const meta: any = cell.column.columnDef.meta;
-                return (
-                  <Td key={cell.id} fontSize="md" py="1" {...meta?.props}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </Td>
-                );
-              })}
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
-    </TableContainer>
+    <VStack>
+      <TableContainer rounded="md" boxShadow="xs" whiteSpace="normal">
+        <Table variant="simple" layout="fixed" width="max-content">
+          <Thead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <Tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                  const meta: any = header.column.columnDef.meta;
+                  return (
+                    <Th key={header.id} {...meta?.thProps}>
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                    </Th>
+                  );
+                })}
+              </Tr>
+            ))}
+          </Thead>
+          <Tbody>
+            {table.getRowModel().rows.map((row) => (
+              <Tr key={row.id} minHeight="96">
+                {row.getVisibleCells().map((cell) => {
+                  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                  const meta: any = cell.column.columnDef.meta;
+                  return (
+                    <Td key={cell.id} fontSize="md" py="1" {...meta?.tdProps}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </Td>
+                  );
+                })}
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+      <HStack>
+        <IconButton
+          aria-label="jump first page"
+          onClick={() => table.setPageIndex(0)}
+          disabled={!table.getCanPreviousPage()}
+          icon={<HiOutlineChevronDoubleLeft />}
+          fontSize="20"
+        />
+        <IconButton
+          aria-label="jump previous page"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+          icon={<HiOutlineChevronLeft />}
+          fontSize="20"
+        />
+        <Button fontWeight="normal">1</Button>
+        <Button fontWeight="normal">2</Button>
+        <Button fontWeight="normal">3</Button>
+        <IconButton
+          aria-label="jump next page"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+          icon={<HiOutlineChevronRight />}
+          fontSize="20"
+        />
+        <IconButton
+          aria-label="jump last page"
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+          disabled={!table.getCanNextPage()}
+          icon={<HiOutlineChevronDoubleRight />}
+          fontSize="20"
+        />
+      </HStack>
+    </VStack>
   );
 };
 export default CustomLinkTable;
