9bab89450887b84a9e8501a4a8d2c324c7b01bf0
eetann <hideharu092@gmail.com>
2023-02-19 15:35:18 +0900

    refactor: e2e #83

--- e2e/customLinkCollection.spec.ts
+++ e2e/customLinkCollection.spec.ts
@@ -1,24 +1,31 @@
+import { goOptionsPage } from "./customLinkHelper.js";
 import { test, expect } from "./fixtures.js";
+import { locator } from "./helper.js";
 import { Page } from "@playwright/test";
 import { readFileSync } from "fs";
 
+const MOCK_JSON5_URL =
+  "https://raw.githubusercontent.com/eetann/choomame-custom-link-collection/main/src/eetann.json5";
+
 async function testCollectionUpdate(page: Page) {
-  await test.step("check @ option page", async () => {
-    // Collection
+  await test.step("check `collection update` @ options", async () => {
+    // check whether Collection was updated
     await expect(
       page.locator("_react=CustomLinkCollectionTable")
     ).not.toHaveText(/eetann-E2E-before/);
     await expect(page.locator("_react=CustomLinkCollectionTable")).toHaveText(
       /eetann-E2E-after/
     );
-    // customLink
-    // beforeOnly
+
+    // check whether Item was updated
+    // does not exist beforeOnly
     await expect(page.locator("_react=CustomLinkTable")).not.toHaveText(
       new RegExp("https://hub-eetann.vercel.app")
     );
+    // exist
     for (const includeText of [
       "https://note.com/hideharu092", // afterOnly
-      "https://zenn.dev/eetann", //same
+      "https://zenn.dev/eetann", // same
       "ツイッター", // update
     ]) {
       await expect(page.locator("_react=CustomLinkTable")).toHaveText(
@@ -28,31 +35,26 @@ async function testCollectionUpdate(page: Page) {
 
     // check whether `enable` is still false
     await expect(
-      page.locator(
-        [
-          "_react=CustomLinkTable",
-          "tr:has-text('eetann|choomame|えーたん')",
-          "input[type=checkbox] ~ span",
-        ].join(" >> ")
-      )
+      locator(page, [
+        "_react=CustomLinkTable",
+        "tr:has-text('eetann|choomame|えーたん')",
+        "input[type=checkbox] ~ span",
+      ])
     ).not.toBeChecked();
   });
 }
 
 test.beforeAll(async ({ context }) => {
-  await context.route(
-    "https://raw.githubusercontent.com/eetann/choomame-custom-link-collection/main/src/eetann.json5",
-    async (route) => {
-      const mockRawData = readFileSync("./e2e/eetann-before-update.json5");
-      await route.fulfill({
-        body: mockRawData,
-      });
-    }
-  );
+  await context.route(MOCK_JSON5_URL, async (route) => {
+    const mockRawData = readFileSync("./e2e/eetann-before-update.json5");
+    await route.fulfill({
+      body: mockRawData,
+    });
+  });
 });
 
 test("CustomLink Collection", async ({ page, extensionId, context }) => {
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(1000);
   await page.goto("https://www.google.com/search?q=typescript+record");
 
   await test.step("onInstalled", async () => {
@@ -61,19 +63,15 @@ test("CustomLink Collection", async ({ page, extensionId, context }) => {
     );
   });
 
-  await page.goto(`chrome-extension://${extensionId}/index.html`);
-  await page.locator(["_react=App", "text='Custom Link'"].join(" >> ")).click();
+  await goOptionsPage(page, extensionId);
 
   await test.step("error handling: zod schema", async () => {
     await page.getByTestId("open-popover-for-new-collection").click();
     await page.locator("#customLinkCollectionURL").fill("aaaa");
-    await page
-      .locator(
-        ["_react=CustomLinkCollectionForm", "button:has-text('Save')"].join(
-          " >> "
-        )
-      )
-      .click();
+    await locator(page, [
+      "_react=CustomLinkCollectionForm",
+      "button:has-text('Save')",
+    ]).click();
     await expect(page.locator("#customLinkCollectionURL ~ div")).toHaveText(
       /Collection URL is invalid/
     );
@@ -85,31 +83,21 @@ test("CustomLink Collection", async ({ page, extensionId, context }) => {
       .fill(
         "https://raw.githubusercontent.com/eetann/choomame-custom-link-collection/main/src/developer.json5555"
       );
-    await page
-      .locator(
-        ["_react=CustomLinkCollectionForm", "button:has-text('Save')"].join(
-          " >> "
-        )
-      )
-      .click();
+    await locator(page, [
+      "_react=CustomLinkCollectionForm",
+      "button:has-text('Save')",
+    ]).click();
     await expect(page.locator("#customLinkCollectionURL ~ div")).toHaveText(
       /The JSON5 in this URL is an invalid format/
     );
   });
 
   await test.step("add Collection", async () => {
-    await page
-      .locator("#customLinkCollectionURL")
-      .fill(
-        "https://raw.githubusercontent.com/eetann/choomame-custom-link-collection/main/src/eetann.json5"
-      );
-    await page
-      .locator(
-        ["_react=CustomLinkCollectionForm", "button:has-text('Save')"].join(
-          " >> "
-        )
-      )
-      .click();
+    await page.locator("#customLinkCollectionURL").fill(MOCK_JSON5_URL);
+    await locator(page, [
+      "_react=CustomLinkCollectionForm",
+      "button:has-text('Save')",
+    ]).click();
     await expect(page.locator("_react=CustomLinkCollectionTable")).toHaveText(
       /eetann-E2E-before/
     );
@@ -123,7 +111,7 @@ test("CustomLink Collection", async ({ page, extensionId, context }) => {
     }
   });
 
-  await test.step("check @ content script", async () => {
+  await test.step("check `add Collection` @ content", async () => {
     await page.goto("https://www.google.com/search?q=eetann");
     await expect(page.locator("#choomameCustomItemLink")).toHaveText(/eetann/);
     for (const customLink of ["Portfolio", "GitHub"]) {
@@ -133,29 +121,21 @@ test("CustomLink Collection", async ({ page, extensionId, context }) => {
     }
   });
 
-  await page.goto(`chrome-extension://${extensionId}/index.html`);
-  await page.locator(["_react=App", "text='Custom Link'"].join(" >> ")).click();
+  await goOptionsPage(page, extensionId);
 
   await test.step("update Collection", async () => {
     // to check `enable` status
-    await page
-      .locator(
-        [
-          "_react=CustomLinkTable",
-          "tr:has-text('eetann|choomame|えーたん')",
-          "input[type=checkbox] ~ span",
-        ].join(" >> ")
-      )
-      .click();
-    await page.route(
-      "https://raw.githubusercontent.com/eetann/choomame-custom-link-collection/main/src/eetann.json5",
-      async (route) => {
-        const mockRawData = readFileSync("./e2e/eetann-after-update.json5");
-        await route.fulfill({
-          body: mockRawData,
-        });
-      }
-    );
+    await locator(page, [
+      "_react=CustomLinkTable",
+      "tr:has-text('eetann|choomame|えーたん')",
+      "input[type=checkbox] ~ span",
+    ]).click();
+    await page.route(MOCK_JSON5_URL, async (route) => {
+      const mockRawData = readFileSync("./e2e/eetann-after-update.json5");
+      await route.fulfill({
+        body: mockRawData,
+      });
+    });
     await page.locator("button:has-text('Manual Update')").click();
     await expect(
       page.locator("button:has-text('Manual Updating')")
@@ -164,19 +144,14 @@ test("CustomLink Collection", async ({ page, extensionId, context }) => {
 
   await testCollectionUpdate(page);
 
-  await page.goto(`chrome-extension://${extensionId}/index.html`);
-  await page.locator(["_react=App", "text='Custom Link'"].join(" >> ")).click();
+  await goOptionsPage(page, extensionId);
 
   await test.step("remove Collection", async () => {
-    await page
-      .locator(
-        [
-          "_react=CustomLinkCollectionTable",
-          "tr:has-text('eetann.json5')",
-          "_react=[aria-label = 'Delete custom link collection']",
-        ].join(" >> ")
-      )
-      .click();
+    await locator(page, [
+      "_react=CustomLinkCollectionTable",
+      "tr:has-text('eetann.json5')",
+      "_react=[aria-label = 'Delete custom link collection']",
+    ]).click();
     await expect(
       page.locator("_react=CustomLinkCollectionTable")
     ).not.toHaveText(/eetann.json5/);
@@ -190,7 +165,7 @@ test("CustomLink Collection", async ({ page, extensionId, context }) => {
     }
   });
 
-  await test.step("check @ content script", async () => {
+  await test.step("check `remove Collection` @ content", async () => {
     await page.goto("https://www.google.com/search?q=eetann");
     await expect(page.locator("#choomameCustomItemLink")).not.toHaveText(
       /eetann/
@@ -202,44 +177,29 @@ test("CustomLink Collection", async ({ page, extensionId, context }) => {
     }
   });
 
-  await page.goto(`chrome-extension://${extensionId}/index.html`);
-  await page.locator(["_react=App", "text='Custom Link'"].join(" >> ")).click();
+  await goOptionsPage(page, extensionId);
 
   await test.step("Auto update", async () => {
     // add
     await page.getByTestId("open-popover-for-new-collection").click();
-    await page
-      .locator("#customLinkCollectionURL")
-      .fill(
-        "https://raw.githubusercontent.com/eetann/choomame-custom-link-collection/main/src/eetann.json5"
-      );
-    await page
-      .locator(
-        ["_react=CustomLinkCollectionForm", "button:has-text('Save')"].join(
-          " >> "
-        )
-      )
-      .click();
-    await page
-      .locator(
-        [
-          "_react=CustomLinkTable",
-          "tr:has-text('eetann|choomame|えーたん')",
-          "input[type=checkbox] ~ span",
-        ].join(" >> ")
-      )
-      .click();
+    await page.locator("#customLinkCollectionURL").fill(MOCK_JSON5_URL);
+    await locator(page, [
+      "_react=CustomLinkCollectionForm",
+      "button:has-text('Save')",
+    ]).click();
+    await locator(page, [
+      "_react=CustomLinkTable",
+      "tr:has-text('eetann|choomame|えーたん')",
+      "input[type=checkbox] ~ span",
+    ]).click();
 
     // mock
-    await page.route(
-      "https://raw.githubusercontent.com/eetann/choomame-custom-link-collection/main/src/eetann.json5",
-      async (route) => {
-        const mockRawData = readFileSync("./e2e/eetann-after-update.json5");
-        await route.fulfill({
-          body: mockRawData,
-        });
-      }
-    );
+    await page.route(MOCK_JSON5_URL, async (route) => {
+      const mockRawData = readFileSync("./e2e/eetann-after-update.json5");
+      await route.fulfill({
+        body: mockRawData,
+      });
+    });
 
     // background updating
     let [background] = context.serviceWorkers();
--- e2e/customLinkItem.spec.ts
+++ e2e/customLinkItem.spec.ts
@@ -1,36 +1,31 @@
-import { customLinkRestoreJsonSchema } from "../src/features/customLink/customLinkSchema.js";
+import { addCustomLinkItem, goOptionsPage } from "./customLinkHelper.js";
 import { test, expect } from "./fixtures.js";
-import fs from "fs";
-import JSON5 from "json5";
-
-const filePath = `test-results/choomame-custom-link.json5`;
+import { locator } from "./helper.js";
 
 test("CustomLink Item", async ({ page, extensionId }) => {
   await page.goto("https://www.google.com/search?q=javascript+foreach");
 
-  await test.step("check URL", async () => {
+  await test.step("check `URL conversion` @ content", async () => {
     // check googleWithURL
     await expect(
-      page.locator(["#choomameCustomItemLink", "a:has(svg)"].join(" >> "))
+      locator(page, ["#choomameCustomItemLink", "a:has(svg)"])
     ).toHaveAttribute(
       "href",
       "https://www.google.com/search?q=site:developer.mozilla.org/en-US/docs/Web/JavaScript foreach"
     );
     // check search in the site
     await expect(
-      page.locator(
-        ["#choomameCustomItemLink", "a:has-text('Search in Reference')"].join(
-          " >> "
-        )
-      )
+      locator(page, [
+        "#choomameCustomItemLink",
+        "a:has-text('Search in Reference')",
+      ])
     ).toHaveAttribute(
       "href",
       "https://developer.mozilla.org/en-US/search?q=foreach"
     );
   });
 
-  await page.goto(`chrome-extension://${extensionId}/index.html`);
-  await page.locator(["_react=App", "text='Custom Link'"].join(" >> ")).click();
+  await goOptionsPage(page, extensionId);
 
   await test.step("error handling: zod schema", async () => {
     await page.getByTestId("open-popover-for-new-item").click();
@@ -48,36 +43,18 @@ test("CustomLink Item", async ({ page, extensionId }) => {
       .fill("a");
     await page.locator("_react=CustomLinkItemForm").getByLabel("URL").fill("a");
     await expect(
-      page.locator(["_react=CustomLinkItemForm", "text='Save'"].join(" >> "))
+      locator(page, ["_react=CustomLinkItemForm", "text='Save'"])
     ).toBeDisabled();
     await expect(page.locator("div:has(> #urlInput) + div")).toHaveText(
       /URL is invalid./
     );
   });
 
-  await test.step("add new CustomLink", async () => {
-    await page
-      .locator("_react=CustomLinkItemForm")
-      .getByLabel("Group name")
-      .fill("Test group");
-    await page
-      .locator("_react=CustomLinkItemForm")
-      .getByLabel("Match")
-      .fill(".*");
-    await page
-      .locator("_react=CustomLinkItemForm")
-      .getByLabel("Link name")
-      .fill("eetann GitHub");
-    await page
-      .locator("_react=CustomLinkItemForm")
-      .getByLabel("URL")
-      .fill("https://github.com/eetann");
-    await page
-      .locator(["_react=CustomLinkItemForm", "text='Save'"].join(" >> "))
-      .click();
+  await test.step("add CustomLink item", async () => {
+    await addCustomLinkItem(page);
   });
 
-  await test.step("check @ form", async () => {
+  await test.step("check `add CustomLink item` @ form", async () => {
     await expect(
       page.locator("_react=CustomLinkItemForm").getByLabel("Group name")
     ).toHaveValue("Test group");
@@ -92,21 +69,17 @@ test("CustomLink Item", async ({ page, extensionId }) => {
     ).toHaveValue("");
   });
 
-  await test.step("toggle customLink", async () => {
-    await page
-      .locator(
-        [
-          "_react=CustomLinkTable",
-          "tr:has-text('Homepagehttps://www.typescriptlang.org')",
-          "input[type=checkbox] ~ span",
-        ].join(" >> ")
-      )
-      .click();
+  await test.step("toggle customLink item", async () => {
+    await locator(page, [
+      "_react=CustomLinkTable",
+      "tr:has-text('Homepagehttps://www.typescriptlang.org')",
+      "input[type=checkbox] ~ span",
+    ]).click();
   });
 
   await page.goto("https://www.google.com/search?q=typescript+record");
 
-  await test.step("check @ content script", async () => {
+  await test.step("check `add and toggle customLink item` @ content", async () => {
     // check add
     await expect(page.locator("#choomameCustomItemLink")).toHaveText(
       /Test group/
@@ -120,174 +93,24 @@ test("CustomLink Item", async ({ page, extensionId }) => {
     );
   });
 
-  await page.goto(`chrome-extension://${extensionId}/index.html`);
-  await page.locator(["_react=App", "text='Custom Link'"].join(" >> ")).click();
+  await goOptionsPage(page, extensionId);
 
-  await test.step("export customLink", async () => {
-    const [download] = await Promise.all([
-      page.waitForEvent("download"),
-      page
-        .locator(
-          ["_react=CustomLinkTab", "button:has-text('Export')"].join(" >> ")
-        )
-        .click(),
-    ]);
-    if (fs.existsSync(filePath)) {
-      try {
-        fs.unlinkSync(filePath);
-      } catch (err) {
-        console.error(err);
-      }
-    }
-    await download.saveAs(filePath);
-    expect(fs.existsSync(filePath)).toBeTruthy();
-
-    const actualFile = fs.readFileSync(filePath);
-    const actualJson5 = JSON5.parse(actualFile.toString());
-    const result = customLinkRestoreJsonSchema.parse(actualJson5);
-    expect(result.id).toBe("user");
-    expect(result.name).toBe("user");
-    const [actualCustomLink] = result.items.filter(
-      (item) => item.name === "eetann GitHub"
-    );
-    expect(actualCustomLink.id).toMatch(/^user\/.*/);
-    expect(actualCustomLink.group).toBe("Test group");
-    expect(actualCustomLink.match).toBe(".*");
-    expect(actualCustomLink.url).toBe("https://github.com/eetann");
-    expect(actualCustomLink.enable).toBe(true);
-    const [actualCollection1] = result.collection.filter(
-      (collection) =>
-        collection.url ===
-        "https://raw.githubusercontent.com/eetann/choomame-custom-link-collection/main/src/choomame-e2e.json5"
-    );
-    expect(actualCollection1.disableIds).toEqual([
-      "chroomame-e2e/typescript-homepage",
-    ]);
-  });
-
-  await test.step("remove customLink", async () => {
-    await page
-      .locator(
-        [
-          "_react=CustomLinkTable",
-          "tr:has-text('Test group')",
-          "_react=[aria-label = 'Delete customLink']",
-        ].join(" >> ")
-      )
-      .click();
+  await test.step("remove customLink item", async () => {
+    await locator(page, [
+      "_react=CustomLinkTable",
+      "tr:has-text('Test group')",
+      "_react=[aria-label = 'Delete customLink']",
+    ]).click();
     await expect(page.locator("_react=CustomLinkTable")).not.toHaveText(
       /Test group/
     );
   });
 
-  await test.step("check @ content script", async () => {
-    await page.goto("https://www.google.com/search?q=typescript+record");
+  await page.goto("https://www.google.com/search?q=typescript+record");
+
+  await test.step("check `remove customLink item` @ content", async () => {
     await expect(page.locator("#choomameCustomItemLink")).not.toHaveText(
       /Test group/
     );
-  });
-
-  await test.step("add new CustomLink for reset", async () => {
-    await page.goto(`chrome-extension://${extensionId}/index.html`);
-    await page
-      .locator(["_react=App", "text='Custom Link'"].join(" >> "))
-      .click();
-
-    await page.getByTestId("open-popover-for-new-item").click();
-    await page
-      .locator("_react=CustomLinkItemForm")
-      .getByLabel("Group name")
-      .fill("Test group");
-    await page
-      .locator("_react=CustomLinkItemForm")
-      .getByLabel("Match")
-      .fill(".*");
-    await page
-      .locator("_react=CustomLinkItemForm")
-      .getByLabel("Link name")
-      .fill("eetann GitHub");
-    await page
-      .locator("_react=CustomLinkItemForm")
-      .getByLabel("URL")
-      .fill("https://github.com/eetann");
-    await page
-      .locator(["_react=CustomLinkItemForm", "text='Save'"].join(" >> "))
-      .click();
-  });
-
-  await test.step("reset customLink", async () => {
-    await page
-      .locator(
-        ["_react=ResetButton[name = 'Custom Link']", "text='Reset'"].join(
-          " >> "
-        )
-      )
-      .click();
-    await page.locator("text='Yes, reset.'").click();
-  });
-
-  await test.step("check @ option page", async () => {
-    // Collection
-    await expect(
-      page.locator("_react=CustomLinkCollectionTable")
-    ).not.toHaveText(/eetann.json5/);
-    // customLink
-    for (const url of [
-      "https://hub-eetann.vercel.app",
-      "https://chrome.google.com/webstore/detail/lecnbgonlcmmpkpnngbofggjiccbnokn",
-    ]) {
-      await expect(page.locator("_react=CustomLinkTable")).not.toHaveText(
-        new RegExp(url)
-      );
-    }
-  });
-
-  await test.step("check @ content script", async () => {
-    await page.goto("https://www.google.com/search?q=typescript+record");
-    // check for add
-    await expect(page.locator("#choomameCustomItemLink")).not.toHaveText(
-      /Test group/
-    );
-    // check for toggle
-    await expect(page.locator("#choomameCustomItemLink")).toHaveText(
-      new RegExp("Homepage")
-    );
-  });
-
-  await page.goto(`chrome-extension://${extensionId}/index.html`);
-  await page.locator(["_react=App", "text='Custom Link'"].join(" >> ")).click();
-
-  await test.step("import customLink", async () => {
-    const [fileChooser] = await Promise.all([
-      page.waitForEvent("filechooser"),
-      page
-        .locator(
-          ["_react=CustomLinkTab", "button:has-text('Import')"].join(" >> ")
-        )
-        .click(),
-    ]);
-    await fileChooser.setFiles(filePath);
-
-    await test.step("check @ option page", async () => {
-      // restore user item
-      await expect(page.locator("_react=CustomLinkTable")).toHaveText(
-        /Test group/
-      );
-      // restore Collection
-      await expect(page.locator("_react=CustomLinkCollectionTable")).toHaveText(
-        /choomame-e2e.json5/
-      );
-
-      // disable
-      await expect(
-        page.locator(
-          [
-            "_react=CustomLinkTable",
-            "tr:has-text('Homepage')",
-            "input[type=checkbox] ~ span",
-          ].join(" >> ")
-        )
-      ).not.toBeChecked();
-    });
   });
 });
