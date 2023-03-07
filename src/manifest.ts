a887a089316f8378f56153e703c5e7d07373893e
eetann <hideharu092@gmail.com>
2023-03-07 19:42:13 +0900

    feat:! delete `( alpha )` in title #87

--- src/manifest.ts
+++ src/manifest.ts
@@ -2,7 +2,7 @@ import { defineManifest } from "@crxjs/vite-plugin";
 
 export default defineManifest({
   manifest_version: 3,
-  name: "Choomame (Alpha)",
+  name: "Choomame",
   description: "quick information at hand",
   version: "1.0.1",
   permissions: ["unlimitedStorage", "storage", "alarms"],
